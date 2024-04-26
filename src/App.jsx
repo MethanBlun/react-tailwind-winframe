f31a528e87778dcd71f735d2451a506313393a6c
Osirus Jack <methanblun@gmail.com>
2024-04-26 06:31:54 +0000

    nextUi autocompleter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {Avatar} from "@nextui-org/react";
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-  
-    </>
+    <div className="flex gap-3 items-center">
+    <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+    <Avatar name="Junior" />
+    <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+    <Avatar name="Jane" />
+    <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+    <Avatar name="Joe" />
+  </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,43 @@
-const {nextui} = require('@nextui-org/theme');
-/** @type {import('tailwindcss').Config} */
-export default {
+// const {nextui} = require('@nextui-org/theme');
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "//     ./index.html",
+    "//     ./src/**/*.{js,ts,jsx,tsx}",
+    "//",
+    "./node_modules/@nextui-org/theme/dist/components/(autocomplete|button|ripple|spinner|input|listbox|divider|popover|scroll-shadow).js"
   ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
- plugins: [nextui()],
-}
+//   theme: {
+//     extend: {},
+//   },
+//   darkMode: "class",
+//  plugins: [nextui()],
+// }
 
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {}, // dark theme colors
+        },
+        // ... custom themes
+      },
+    }),
+  ],
+};
