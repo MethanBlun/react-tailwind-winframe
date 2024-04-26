9eea35ac4a378459bf8f6ba9c85598b7d39ae173
Osirus Jack <methanblun@gmail.com>
2024-04-26 04:54:43 +0000

    add all next components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [nextui()],
+  darkMode: "class",
+ plugins: [nextui()],
 }
 
